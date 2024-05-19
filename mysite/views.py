55aab5cfe845a3c61d12d7566e4b47ed30c60249
takuto-abc <144294967+takuto-abc@users.noreply.github.com>
2024-05-19 10:51:14 +0900

    commit_first_app
    Blog Application

---
+++ mysite/views.py
@@ -0,0 +1,45 @@
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from blog.models import Article
+from django.contrib.auth.views import LoginView
+from mysite.forms import UserCreationForm
+from django.contrib import messages
+
+
+def index(request):
+    objs = Article.objects.all()[:3]
+    context = {
+        'title': 'Really Site',
+        'articles': objs,
+    }
+    
+    return render(request, 'mysite/index.html', context)
+
+
+class Login(LoginView):
+    template_name = 'mysite/auth.html'
+
+    def form_valid(self, form):
+        messages.success(self.request, 'ログイン完了！！！')    
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'エラーあり！！！')
+        return super().form_invalid(form)
+
+def signup(request):
+    context = {}
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            #user.is_active = False
+            user.save()
+            messages.success(request, '登録完了！！！')
+           
+            return redirect('/')
+            
+    return render(request, 'mysite/auth.html', context)
+
